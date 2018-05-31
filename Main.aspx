8def4453a7c3cbc577d729065e277167804a7c3d
Mantas <Tidish@inbox.lt>
2018-06-01 00:26:49 +0300

    Start of project, first commit

---
+++ Main.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Idle heroes fusion</title>
+    <link rel="stylesheet" href="/css/stylesheet.css"/>
+</head>
+<body>
+    <form id="MainForm" runat="server">
+        <div class="container">
+            <div id="Images">
+                <div class="DropDownList">
+                    <asp:DropDownList ID="DropDownListFactions" runat="server" OnSelectedIndexChanged="DropDownListFactionsChanged" AutoPostBack="True">
+                    <asp:ListItem>Shadow</asp:ListItem>
+                    <asp:ListItem>Fortress</asp:ListItem>
+                    <asp:ListItem>Abyss</asp:ListItem>
+                    <asp:ListItem>Forest</asp:ListItem>
+                    <asp:ListItem>Dark</asp:ListItem>
+                    <asp:ListItem>Light</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Label ID="ErrorLabel" runat="server" Text=""></asp:Label>
+                 </div>
+                <div class="ImageForm">
+                    <asp:Image ID="Image1" CssClass="ImageFaces" runat="server" />
+                    <asp:Label ID="Label1" CssClass="ImageLabels" runat="server" Text=""></asp:Label>
+                    <asp:TextBox ID="Spinner1" TextMode="Number" runat="server" CssClass="ImageSpinner" value="0" type="number" min="0" max="200" />
+                    <asp:TextBox ID="Spinner11" TextMode="Number" runat="server" CssClass="ImageSpinner" value="0" step="20" type="number" min="0" max="1000" /><br />
+                </div>
+                
+                <div class="ImageForm">
+                    <asp:Image ID="Image2" CssClass="ImageFaces" runat="server" />
+                    <asp:Label ID="Label2" CssClass="ImageLabels" runat="server" Text=""></asp:Label>
+                    <asp:TextBox ID="Spinner2" TextMode="Number" runat="server" CssClass="ImageSpinner" value="0" type="number" min="0" max="200" />
+                    <asp:TextBox ID="Spinner22" TextMode="Number" runat="server" CssClass="ImageSpinner" value="0" step="20" type="number" min="0" max="1000" /><br />
+                </div>
+           
+
+            </div>
+            <asp:Button ID="ImageButton" runat="server" Text="Submit" />
+        </div>
+
+    </form>
+
+</body>
+</html>
---
+++ Main.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DisplayItems("Shadow");
+        }
+    }
+
+    protected void DropDownListFactionsChanged(object sender, EventArgs e)
+    {
+        DropDownList btn = sender as DropDownList;
+        DisplayItems(btn.Text);
+
+    }
+
+    protected void DisplayItems(string option)
+    {
+        switch (option)
+        {
+            case "Shadow":
+                Image1.ImageUrl = "~/pictures/heroes-face/chain-hammer-icon.png";
+                Label1.Text = "Chain hammer";
+                Image2.ImageUrl = "~/pictures/heroes-face/dark-priest-icon.png";
+                Label2.Text = "Dark priest";
+                break;
+            case "Fortress":
+                Image1.ImageUrl = "~/pictures/heroes-face/reggie-icon.png";
+                Label1.Text = "Chain hammer";
+                Image2.ImageUrl = "~/pictures/heroes-face/fire-fist-icon.png";
+                Label2.Text = "Dark priest";
+                break;
+            case "Abyss":
+                Image1.ImageUrl = "~/pictures/heroes-face/chain-hammer-icon.png";
+                Label1.Text = "Chain hammer";
+                Image2.ImageUrl = "~/pictures/heroes-face/dark-priest-icon.png";
+                Label2.Text = "Dark priest";
+                break;
+            case "Forest":
+                Image1.ImageUrl = "~/pictures/heroes-face/chain-hammer-icon.png";
+                Label1.Text = "Chain hammer";
+                Image2.ImageUrl = "~/pictures/heroes-face/dark-priest-icon.png";
+                Label2.Text = "Dark priest";
+                break;
+            case "Dark":
+                Image1.ImageUrl = "~/pictures/heroes-face/chain-hammer-icon.png";
+                Label1.Text = "Chain hammer";
+                Image2.ImageUrl = "~/pictures/heroes-face/dark-priest-icon.png";
+                Label2.Text = "Dark priest";
+                break;
+            case "Light":
+                Image1.ImageUrl = "~/pictures/heroes-face/chain-hammer-icon.png";
+                Label1.Text = "Chain hammer";
+                Image2.ImageUrl = "~/pictures/heroes-face/dark-priest-icon.png";
+                Label2.Text = "Dark priest";
+                break;
+            default:
+                ErrorLabel.Text = "Unknown faction";
+                break;
+        }
+                                      
+    }
+}
